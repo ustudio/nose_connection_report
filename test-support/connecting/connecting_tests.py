f41308875f44156c0fc976cbc1af66a54210d3d1
Thomas Stephens <thomas@ustudio.com>
2014-02-25 16:04:26 -0600

    Explicitly connecting to IP address, to try to make connect be less fancy

--- test-support/connecting/connecting_tests.py
+++ test-support/connecting/connecting_tests.py
@@ -3,14 +3,14 @@ import socket
 
 def connecting_test_1():
     try:
-        socket.create_connection(("localhost", 8080))
+        socket.create_connection(("127.0.0.1", 8080))
     except:
         pass
 
 
 def connecting_test_2():
     try:
-        socket.create_connection(("localhost", 4321))
+        socket.create_connection(("127.0.0.1", 4321))
     except:
         pass
 
